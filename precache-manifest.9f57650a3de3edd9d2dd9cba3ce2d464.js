d4e2b0e85629accabf5cc7f1dd0355e266198b33
zahidalidev <engrzahid612@gmail.com>
2020-07-26 02:40:32 +0500

    current and custom screenshot

---
+++ precache-manifest.9f57650a3de3edd9d2dd9cba3ce2d464.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f0fd95518470f237cc7daab534fc86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3310b127a78246f3b5d",
+    "url": "/static/css/main.0f642f61.chunk.css"
+  },
+  {
+    "revision": "2a1e4d906c7953014d6a",
+    "url": "/static/js/2.3485cebe.chunk.js"
+  },
+  {
+    "revision": "2d44b0d619a12c1417728a085056f2e7",
+    "url": "/static/js/2.3485cebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3310b127a78246f3b5d",
+    "url": "/static/js/main.5c0cf628.chunk.js"
+  },
+  {
+    "revision": "33ee15faf6d3c613841b",
+    "url": "/static/js/runtime-main.6cfd77cd.js"
+  },
+  {
+    "revision": "3981ffe79d6c470c2de3b259a2553935",
+    "url": "/static/media/webShot.3981ffe7.png"
+  }
+]);
